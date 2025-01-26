57a18efd15a6949a145fb9e46246cf635663e15d
GustavoDiaso <kingclash1906@gmail.com>
2025-01-26 16:06:45 -0300

    Aprendendo o Básico de WebScraping

--- aulas/secao_modulos/modulo_http/main.py
+++ aulas/secao_modulos/modulo_http/main.py
@@ -18,3 +18,42 @@
 # - Os cabeçalhos HTTP (Content-Type, Accept)
 # - O corpo da mensagem (Pode estar em vazio em alguns casos)
 
+# O módulo requests consegue carregar dados da Internet para dentro do seu
+# código. Já o bs4.BeautifulSoup é responsável por interpretar os dados HTML
+# em formato de objetos Python para facilitar a vida do desenvolvedor.
+
+# Tutorial Requests -> https://youtu.be/Qd8JT0bnJGs
+# Biblioteca Beautiful Soup  - Doc: https://www.crummy.com/software/BeautifulSoup/bs4/doc.ptbr/
+
+# modulo para fazer requisições
+import requests
+from bs4 import BeautifulSoup
+import os
+
+url = "http://localhost:3333/"
+
+response = requests.get(url=url)
+raw_html = response.text
+# print("Status Code:", response.status_code)
+# print(raw_html)
+
+# Html em formato objeto python
+parsed_html = BeautifulSoup(raw_html, "html.parser", from_encoding="utf-8")
+
+print(parsed_html.title)
+print(parsed_html.title.text)
+
+top_jobs_heading = parsed_html.select_one("#intro > div > div > article > h2")
+print(top_jobs_heading.text)
+
+# pegando elemento pai do nosso <h2>TOP 3 JOBS<\h2>
+article = top_jobs_heading.parent
+
+if article is not None:
+    for paragraph in article.select("p"):
+        print(paragraph.text.strip())
+
+"""
+-> htmlobject.select_one: retorna apenas um dos objetos com a tag especificada 
+-> htmlobject.select: retorna todos os objetos com a tag especificada.
+"""
